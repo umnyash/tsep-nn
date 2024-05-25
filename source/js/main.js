b7888f6b1e5331df7fe3f2466048c8f6eff3b82d
Rustam Sadreev <rustam17.sb@gmail.com>
2024-05-25 21:26:49 +0300

    feat: add news-selection section

--- source/js/main.js
+++ source/js/main.js
@@ -1,12 +1,8 @@
 /* * * * * * * * * * * * * * * * * * * * * * * *
  * main.js
  */
-// const siteNavigationElement = document.querySelector('.site-navigation');
-// // const siteNavigation = new SiteNavigation(siteNavigationElement);
-// initSiteNavigation(siteNavigationElement);
-
-
 const siteHeaderElement = document.querySelector('.site-header');
 initSiteHeader(siteHeaderElement, initBurger, initSiteNavigation);
-// const siteHeader = new SiteHeader({siteHeaderElement, burBurger});
+
+document.querySelectorAll('.selection--news').forEach(initNewsSelection);
 /* * * * * * * * * * * * * * * * * * * * * * * */
---
+++ source/js/modules/news-selection.js
@@ -0,0 +1,26 @@
+/* * * * * * * * * * * * * * * * * * * * * * * *
+ * news-selection.js
+ */
+function initNewsSelection(newsSelectionElement) {
+  const cursorElement = document.querySelector('.cursor-picture');
+  const cursorImageElement = cursorElement.querySelector('.cursor-picture__image');
+  const linkElements = newsSelectionElement.querySelectorAll('.selection__item-link');
+
+  linkElements.forEach((linkElement) => {
+    linkElement.addEventListener('mouseover', ({ target }) => {
+      const imageUrl = target.parentElement.dataset.cursorImage;
+      cursorImageElement.style.backgroundImage = `url("${imageUrl}")`;
+      cursorElement.classList.remove('cursor-picture--hidden');
+    });
+
+    linkElement.addEventListener('mousemove', (evt) => {
+      cursorElement.style.top = `${evt.pageY}px`;
+      cursorElement.style.left = `${evt.pageX}px`;
+    });
+
+    linkElement.addEventListener('mouseout', () => {
+      cursorElement.classList.add('cursor-picture--hidden');
+    });
+  });
+}
+/* * * * * * * * * * * * * * * * * * * * * * * */
