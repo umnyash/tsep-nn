bf0659f9c6d45ea6bad9328a1babdc59bfa66492
Rustam Sadreev <rustam17.sb@gmail.com>
2024-06-10 02:26:25 +0300

    feat: position the header and adjust the indentation of large blocks

--- source/js/modules/site-header.js
+++ source/js/modules/site-header.js
@@ -12,5 +12,28 @@ function initSiteHeader(siteHeaderElement, initBurger, initSiteNavigation) {
 
   initBurger(burgerElement, myCb);
   initSiteNavigation(siteNavigationElement);
+
+  let pageScrollY = 0;
+
+  const onWindowScroll = () => {
+    const headerHeight = siteHeaderElement.offsetHeight;
+
+    if (window.scrollY > 0) {
+      siteHeaderElement.classList.add('site-header--sticked');
+    }
+
+    if (window.scrollY > pageScrollY && window.scrollY > headerHeight) {
+      siteHeaderElement.classList.add('site-header--hidden');
+    } else {
+      siteHeaderElement.classList.remove('site-header--hidden');
+
+      if (window.scrollY === 0) {
+        siteHeaderElement.classList.remove('site-header--sticked');
+      }
+    }
+    pageScrollY = window.scrollY;
+  };
+
+  window.addEventListener('scroll', onWindowScroll);
 }
 /* * * * * * * * * * * * * * * * * * * * * * * */
