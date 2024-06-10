10c26f548a6af0b2e59f35c9da9840928431b916
Rustam Sadreev <rustam17.sb@gmail.com>
2024-06-10 07:16:01 +0300

    feat: change production slider

--- source/js/modules/production-slider.js
+++ source/js/modules/production-slider.js
@@ -2,17 +2,54 @@
  * products-slider.js
  */
 function initProductionSlider(sliderElement) {
-  const swiper = new Swiper(sliderElement, {
-    direction: 'vertical',
+  const element = sliderElement.querySelector('.production-slider__slider');
+
+  const swiper = new Swiper(element, {
     slidesPerView: 1,
-    mousewheel: {
-      enable: true,
-      releaseOnEdges: true,
-    },
     pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+      el: '.production-slider__pagination',
+      bulletClass: 'production-slider__pagination-item',
+      bulletActiveClass: 'production-slider__pagination-item--active',
     },
+    allowTouchMove: false,
+  });
+
+  let currentThemeClassName = 'null';
+
+  swiper.on('slideChange', () => {
+    sliderElement.classList.remove(currentThemeClassName);
+
+    const slideNumber = swiper.realIndex + 1;
+
+    switch (true) {
+      case slideNumber % 4 === 3:
+        currentThemeClassName = `production-slider--theme_${3}`;
+        break;
+      case slideNumber % 2 === 0:
+        currentThemeClassName = `production-slider--theme_${2}`;
+        break;
+      default:
+        currentThemeClassName = `production-slider--theme_${1}`;
+        break;
+    }
+
+    sliderElement.classList.add(currentThemeClassName);
+  });
+
+  const slidesNumber = swiper.slides.length;
+  const scrollLength = slidesNumber * 300;
+
+  gsap.registerPlugin(ScrollTrigger);
+
+  ScrollTrigger.create({
+    trigger: '.production-slider',
+    start: 'top top',
+    end: `+=${scrollLength}`,
+    pin: true,
+    onUpdate: (self) => {
+      const slideIndex = Math.floor(self.progress * slidesNumber);
+      swiper.slideTo(slideIndex);
+    }
   });
 }
 /* * * * * * * * * * * * * * * * * * * * * * * */
