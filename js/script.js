2dc29a324d0624047a7837647f639d2f7fcdbb20
umnyash <63366308+umnyash@users.noreply.github.com>
2024-06-10 04:44:39 +0000

    Deploying to gh-pages from @ umnyash/tsep-nn@5261da7223c18a5bc8543897dbba4137db9fb72b 🚀

--- js/script.js
+++ js/script.js
@@ -3,17 +3,7 @@
 /* * * * * * * * * * * * * * * * * * * * * * * *
  * const.js
  */
-// const DESKTOP_WIDTH_MEDIA_QUERY = '(min-width: 1346px)';
 const LAPTOP_WIDTH_MEDIA_QUERY = '(min-width: 1260px)';
-// const WIDE_TABLET_WIDTH_MEDIA_QUERY = '(min-width: 1024px)';
-
-// const KeyCode = Object.freeze({
-//   LEFT_ARROW: 'ArrowLeft',
-//   RIGHT_ARROW: 'ArrowRight',
-//   DOWN_ARROW: 'ArrowDown',
-//   SPACE: 'Space',
-//   ESCAPE: 'Escape',
-// });
 /* * * * * * * * * * * * * * * * * * * * * * * */
 
 function createElementByString(template) {
@@ -659,4 +649,5 @@ document.querySelectorAll('form[data-name]').forEach(formElement => {
 document.querySelectorAll('.production-slider').forEach(initProductionSlider);
 document.querySelectorAll('.photo-slider').forEach(initPhotoSlider);
 document.querySelectorAll('.adaptive-table').forEach(initAdaptiveTable);
+document.querySelectorAll('input[type="tel"]').forEach(initTelField);
 /* * * * * * * * * * * * * * * * * * * * * * * */
